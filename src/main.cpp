f8a471275a247ce3a929f3d04c7551047376bf4b
ihor99k <168579989+ihor99k@users.noreply.github.com>
2026-04-09 19:34:22 +0300

    лаба2

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,44 @@
 #define YELLOW 10
 #define RED 11
 #define WHITE 13
-//#define BUZZER 5
+#define BUZZER 5
+#define BUTTON 36
+#define ZERO 0
 
 uint32_t delayTime = 500;
 double devider = 0.99;
 int16_t ledPins[] = {GREEN, BLUE, YELLOW, RED, WHITE};
 int16_t randomLed;
+
+int16_t buttons_PU[] = {ZERO, BUTTON};
+int16_t buttons_State[] = {0, 0};
+uint32_t targetmils[] = {0, 0};
+int8_t initstate_fkmk = 0;
+int buttonState = 0;
 void setup() {
   for (int i = 0; i < 5; i++) {
+    //digitalWrite(ledPins[i], HIGH);
     pinMode(ledPins[i], OUTPUT);
   }
   //pinMode(BUZZER, OUTPUT);
+  
+    pinMode(BUTTON, INPUT_PULLUP);
+    targetmils[1] = millis();
+  targetmils[0] = millis();
+ 
   for (int i = 0; i < 5; i++) {
     digitalWrite(ledPins[i], HIGH);
   }
 }
 
 void loop() {
-  while (delayTime>4)
+  if (initstate_fkmk == 0)
+  {
+    pinMode(buttons_PU[0], INPUT_PULLUP);
+    initstate_fkmk = 1;
+  }
+  
+  /*while (delayTime>4)
   {
     randomLed = random(5);
     digitalWrite(ledPins[randomLed], LOW);
@@ -33,7 +53,48 @@ void loop() {
   }
   
   if (delayTime < 5) {
-        delayTime = 500;
+            delayTime = 500;
+          }
+     */
+    for (int i = 0; i < 2; i++) {
+  int currentState = digitalRead(buttons_PU[i]);
+
+  if (currentState == HIGH) { 
+    // Кнопка відпущена — скидаємо таймер і записуємо HIGH
+    targetmils[i] = millis();
+    buttons_State[i] = HIGH;
+  } 
+  else {
+    // Кнопка натиснута (LOW) — перевіряємо, чи пройшло 20 мс
+    if (millis() - targetmils[i] > 20) {
+      buttons_State[i] = LOW; // Дребезг пройшов, фіксуємо натискання
+    }
+  }
+}
+
+
+      if (buttons_State[1] == LOW) {
+         digitalWrite(BLUE, LOW); //
+         delay(100);
+         digitalWrite(BLUE, HIGH); //
+         delay(100);
+
+      } else {
+        
+        digitalWrite(BLUE, HIGH);
+      }
+if (buttons_State[0] == LOW) {
+         digitalWrite(WHITE, LOW); //
+         delay(1000);
+         digitalWrite(WHITE, HIGH); //
+         delay(1000);
+
+      } else {
+        
+        digitalWrite(WHITE, HIGH);
       }
 
-}
+
+      digitalWrite(YELLOW, LOW);
+   
+  }
